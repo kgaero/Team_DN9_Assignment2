cba61b7418c62bbac975de27e99a838a5fa4cc53
rakeshprusty-purdue <prustyr@purdue.edu>
2025-07-27 17:05:48 -0500

    Updated Rakesh's SQL

--- sql_queries/make_predictions.sql
+++ sql_queries/make_predictions.sql
@@ -6,3 +6,12 @@ SELECT * FROM ML.EVALUATE(MODEL `mgmt599-kunalghosh.assignment2.sales_forecast_m
 
 
 -- ───────── END of SQL queries by Kunal Ghosh (ghosh178@purdue.edu) ───────── -->
+
+-- ───────── START of SQL queries by Rakesh Prusty (prustyr@purdue.edu) ───────── -->
+
+
+-- Check model performance
+SELECT * FROM ML.EVALUATE(MODEL `mgmt599-rakesh-assignment-1.store_sales_team_data_ninja.sales_forecast_model`);
+
+
+-- ───────── END of SQL queries by Rakesh Prusty (prustyr@purdue.edu) ───────── -->
