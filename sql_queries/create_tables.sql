0448020fb341c5faaa8676d04b1bb4844adbad2d
pavankmeka <pmeka@purdue.edu>
2025-07-28 00:08:47 -0400

    Update create_tables.sql with Pavan's updates

--- sql_queries/create_tables.sql
+++ sql_queries/create_tables.sql
@@ -53,3 +53,32 @@ CREATE TABLE IF NOT EXISTS `mgmt599-rakesh-assignment-1.store_sales_team_data_ni
 );
 
 -- ───────── END of SQL queries by Rakesh Prusty (prustyr@purdue.edu) ───────── -->
+
+-- ───────── START of SQL queries by Rakesh Prusty (prustyr@purdue.edu) ───────── -->
+-- 1) oil table
+CREATE TABLE IF NOT EXISTS `mgmt599-pavanmeka-lab1.store_sales_team_DN9.sales_data` (
+  date       DATE,
+  dcoilwtico FLOAT64
+);
+
+
+-- 2) train table
+CREATE TABLE IF NOT EXISTS `mgmt599-pavanmeka-lab1.store_sales_team_DN9.oil` (
+  id           INT64,
+  date         DATE,
+  store_nbr    INT64,
+  family       STRING,
+  sales        FLOAT64,
+  onpromotion  INT64
+);
+
+-- 3) stores table
+CREATE TABLE IF NOT EXISTS `mgmt599-pavanmeka-lab1.store_sales_team_DN9.store_info` (
+  store_nbr INT64,
+  city      STRING,
+  state     STRING,
+  type      STRING,
+  cluster   INT64
+);
+
+-- ───────── END of SQL queries by Pavan Meka (pmeka@purdue.edu) ───────── -->
