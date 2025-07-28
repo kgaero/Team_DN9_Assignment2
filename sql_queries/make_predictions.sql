13d12fad73852e9ab7123a8862d492946a003cac
pavankmeka <pmeka@purdue.edu>
2025-07-28 00:10:20 -0400

    Update make_predictions.sql with Pavan's updates

--- sql_queries/make_predictions.sql
+++ sql_queries/make_predictions.sql
@@ -15,3 +15,12 @@ SELECT * FROM ML.EVALUATE(MODEL `mgmt599-rakesh-assignment-1.store_sales_team_da
 
 
 -- ───────── END of SQL queries by Rakesh Prusty (prustyr@purdue.edu) ───────── -->
+
+-- ───────── START of SQL queries by Pavan Meka (pmeka@purdue.edu) ───────── -->
+
+
+-- Check model performance
+SELECT * FROM ML.FORECAST(MODEL `mgmt599-pavanmeka-lab1.store_sales_team_DN9.sales_forecast_model`,
+                          STRUCT(14 AS horizon));
+
+-- ───────── END of SQL queries by Pavan Meka (pmeka@purdue.edu) ───────── -->
